14a71bfe96eab2f05b879417ca0c7862fc21d50e
Naman Sahu <120280018+Bozonaman@users.noreply.github.com>
2023-05-21 22:56:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+  <>
+  <div className="container " style={{backgroundColor:"lightcoral" , height:"100vh", width:"100vw"}} >
+
+ <Todo/>
+  </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  </>
+  );
+}
+
+export default App;
